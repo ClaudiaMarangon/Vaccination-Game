603e86a34e709f94218f4d9fdd6707539e16ed66
ClaudiaMarangon <claudia.marangon@studbocconi.it>
2018-03-28 16:40:14 +0200

    Added Survey

--- survey/models.py
+++ survey/models.py
@@ -36,4 +36,81 @@ class Player(BasePlayer):
         max_length = 150
     )
 
+    q1 = models.IntegerField(
+        min = 0,
+        max = 100,
+    )
+
+    q2 = models.IntegerField(
+        min = 0,
+        max = 100,
+    )
+
+    q3 = models.StringField(
+        verbose_name = 'Was there anything else you would like to mention regarding the content in the prompt?',
+        blank=True
+    )
+
+    q4 = models.IntegerField(
+        min = 0,
+        max = 100,
+    )
+
+    q5 = models.StringField(
+        verbose_name = 'If in your response to the previous question, you reported that with some probability you may not get vaccinated. Why did you say that?',
+        choices=['religious beliefs', 'side effects', 'vaccine will not work', 'good hygiene is enough', 'because most people will already be vaccinated', 'other'],
+        blank =True,
+    )
+
+    q6 = models.IntegerField(
+        min = 0,
+        max = 100,
+    )
+
+    doc = models.IntegerField(
+        verbose_name= 'Doctor/Pediatrician',
+        choices=[1, 2, 3, 4, 5, 6]
+    )
+
+    par = models.IntegerField(
+        verbose_name='Partner/Family',
+        choices=[1, 2, 3, 4, 5, 6]
+    )
+
+    fre = models.IntegerField(
+        verbose_name='Friends',
+        choices=[1, 2, 3, 4, 5, 6]
+    )
+
+    med = models.IntegerField(
+        verbose_name='Internet/Social Media',
+        choices=[1, 2, 3, 4, 5, 6]
+    )
+
+    tv = models.IntegerField(
+        verbose_name='TV/Newspaper/Radio',
+        choices=[1, 2, 3, 4, 5, 6]
+    )
+
+    oth = models.IntegerField(
+        verbose_name='Other',
+        choices=[1, 2, 3, 4, 5, 6]
+    )
+
+    spec = models.StringField(
+        verbose_name='Please specify where you obtain the information you need to make decisions regarding vaccinations.',
+    )
+
+
+    q7 = models.IntegerField(
+        min = 0,
+        max = 100,
+    )
+
+    q8 = models.IntegerField(
+        min = 0,
+        max = 100,
+    )
+
+
     pass
--- survey/views.py
+++ survey/views.py
@@ -7,10 +7,28 @@ from .models import Constants
 class Strategy(Page):
     form_model = models.Player
     form_fields = ['two_p', 'three_p']
+    pass
 
+class Survey_Extract1(Page):
+    form_model = models.Player
+    form_fields = ['q1', 'q2', 'q3']
+    pass
+
+class Survey_Extract2(Page):
+    form_model = models.Player
+    form_fields = ['q4', 'q5', 'q6', 'q7', 'q8']
+    pass
+
+
+class Survey_Extract3(Page):
+    form_model = models.Player
+    form_fields = ['doc', 'par', 'fre', 'med', 'tv', 'oth', 'spec']
     pass
 
 
 page_sequence = [
     Strategy,
+    Survey_Extract1,
+    Survey_Extract2,
+    Survey_Extract3
 ]
